1f8dc309578a2544eeec2f7f98451cf6af633111
cperry-adv <cperry@advanstar.com>
2018-01-15 13:52:57 -0800

    stuff

--- js/reddrock.js
+++ js/reddrock.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     $("#about").click(function(){
         $(".about-container").toggleClass('hidden visible');
+        $(".contact-container").hide();
     });
     $("#contact").click(function(){
         $(".contact-container").toggleClass('hidden visible');
+        $(".about-container").hide();
     });
 });

f291e0e66e683b4c694a0f8adb7439439e2dfc61
cperry-adv <cperry@advanstar.com>
2018-01-15 14:12:38 -0800

    ya

--- contactthanks.php
+++ contactthanks.php
@@ -1,27 +1,22 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
 
 <head>
-	<title>A Nice & Simple Contact Form</title>
+	<title>Message Sent!</title>
 	
-	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="stylesheet" type="text/css" href="css/reddrock.css" />
 </head>
 
 <body>
-
-	<div id="page-wrap">
-
-		<img src="images/title.gif" alt="A Nice & Simple Contact Form" />
-		<p>By <a href="http://css-tricks.com">CSS-Tricks</a></p>
-			
-			
-		<br /><br />
+<div class="header">
+<a href="mailto:info@reddrockmusic.com"><img src="images/reddlogo.png"></a>
+<h1>Redd Rock International Music Publishing</h1>
+</div>
 		
 			
 		<h1>Your message has been sent!</h1><br />
 		
 		<p><a href="index.html">Back to Contact Form</a></p>
-	
-	</div>
+
 	
 	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
 	</script>

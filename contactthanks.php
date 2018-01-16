7fd91c8673954f804f424eec53619f00f5616ac5
cperry-adv <cperry@advanstar.com>
2018-01-15 16:48:38 -0800

    new image

--- contactthanks.php
+++ contactthanks.php
@@ -22,11 +22,11 @@
 
 </div>
 		
-			
+			<div style="text-align:center;width:100%;">
 		<h1>Your message has been sent!</h1><br />
 		
 		<p><a href="index.html">Back to Contact Form</a></p>
-
+</div>
 	
 	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
 	</script>

aa9e79b2380bcb0536ab0759f70cdc2d5cfdcacf
cperry-adv <cperry@advanstar.com>
2018-01-15 17:27:53 -0800

    ya

--- js/reddrock.js
+++ js/reddrock.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky-header");
+  } else {
+    header.classList.remove("sticky-header");
+  }
+}
+
     $("#about").click(function(){
         $(".about-container").toggleClass('hidden visible');
         if ($(".contact-container").hasClass('visible')) {

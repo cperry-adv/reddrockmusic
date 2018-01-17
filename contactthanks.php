ddb63ab1d7af43458a83825fe6291f30a6c6e1d4
cperry-adv <cperry@advanstar.com>
2018-01-16 20:27:29 -0800

    ya

--- contactthanks.php
+++ contactthanks.php
@@ -1,35 +1,45 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+<!DOCTYPE html>
+<html>
 
 <head>
+	<title>ReddRockMusic | Home</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/reddrock.css">
+    <link rel="stylesheet" href="https://use.typekit.net/grj4fqq.css">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-<title>Message Sent!</title>
-<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-<script src="http://fancywizard.com/js/reddrock.js"></script>
-<link rel="stylesheet" href="css/reddrock.css">
-<link rel="stylesheet" href="https://use.typekit.net/grj4fqq.css">
+	<script src="https://use.fontawesome.com/08498914ef.js"></script>
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	<script src="http://fancywizard.com/js/reddrock.js"></script>
 </head>
 
 <body>
-<div class="header">
+	<div class="wrapper">
+		<div id="header" class="header">
+			<img class="logo" alt="ReddRockMusic" title="ReddRockMusic" src="images/reddcolor.png">
+				<div class="nav">
+					<ul class="header-menu">
+						<li id="about"><a href="#" >About</a></li>
+						<li id="contact"><a href="#" >Contact</a></li>
+					</ul>
+				</div>
 
-<img class="logo" src="images/reddcolor.png">
-
-<div class="nav">
-<ul>
-<li><a href="#" id="about">About</a></li>
-<li><a href="#" id="contact">Contact</a></li>
-</ul>
-</div>
-
-</div>
-		
-			<div style="text-align:center;width:100%;">
-		<h1>Your message has been sent!</h1><br />
-		
-		<p><a href="index.html">Back to Home</a></p>
-</div>
-	
-	
+		</div>
+		<div class="wrap">
+			<div id="main" class="main">
+				<div style="text-align:center;width:100%;">
+		        	<h1>Your message has been sent!</h1><br />
+					<p><a href="index.html">Back to Home</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="push"></div>
+	</div>
+		<div class="footer">
+			<ul class="footer-menu">
+				<li><a href="https://twitter.com/reddrockmusic"><i class="fa fa-2x fa-twitter-square" aria-hidden="true"></i></a></li>
+				<li><a href="https://www.instagram.com/reddrockmusic"><i class="fa fa-2x fa-instagram" aria-hidden="true"></i></a></li>
+			</ul>
+		</div>
 </body>
-
 </html>
